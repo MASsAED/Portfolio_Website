0a02d060d608d49fd0448555dbe1f57f97ac2eb2
masoud saed <masoudsaed918@gmail.com>
2023-02-01 20:58:10 +0000

    js 1

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 
+ function displyNavBar(x){
+ let x = document.getElementById('menu').innerHTML;
+ let y = document.getElementById('menu').addEventListener("click", displyNavBar);
+
+ if (x){
+    x = y;
+ }
+}

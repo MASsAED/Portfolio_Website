87a16952d78625b050fc543be303f67b29c79320
masoud saed <masoudsaed918@gmail.com>
2023-02-01 01:24:54 +0000

    styling-responsive-finished

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-const sharp = require('sharp');
-const fs = require('fs');
-const { AsyncLocalStorage } = require('async_hooks');
-const directory = '';
 
-fs.readdirSync(directory).forEach(file => {
-  sharp(`${directory}/${file}`)
-    .resize(200, 100) // width, height
-    .toFile(`${directory}/${file}-small.jpg`);
-  });
-
-  function myFunction() {
-    alert("Hello\nHow are you?");
-  }
 

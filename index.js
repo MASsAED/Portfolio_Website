dc0bf9420793f018fe459c888f4894aeed2955a2
masoud saed <masoudsaed918@gmail.com>
2023-02-03 00:05:40 +0000

    styling and interactive site

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+/* This Function made 3-line img menu showing topnav Items */
+function displayNavbar(){
+    var x = document.getElementById("topnav");
+    var y = document.getElementsByClassName("nav_bar");
+    
+    if (x.style.display === "inline-block"){
+        x.style.display = "none";
+    } else {
+        x.style.display = "inline-block";
+        x.style.backgroundColor = '#e5e5e5';
+        x.style.width = '100%';
+        x.style.textAlign = 'center';
+        x.style.padding = '0.5rem';
+    }
+    }
 
-
- function displyNavBar(x){
- let x = document.getElementById('menu').innerHTML;
- let y = document.getElementById('menu').addEventListener("click", displyNavBar);
-
- if (x){
-    x = y;
- }
-}
+/* -- */
